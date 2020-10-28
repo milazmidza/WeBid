9cae5914bbabe58f21d266193ab53d0add4c6f9b
Milton <gorechinoda@gmail.com>
2020-10-28 11:02:02 +0000

    Fuck

--- includes/config.inc12.php
+++
@@ -1,9 +0,0 @@
-<?php
-$DbHost	 = "localhost";
-$DbDatabase = "auction";
-$DbUser	 = "root";
-$DbPassword = "";
-$DBPrefix	= "webid_";
-$main_path = "C:\\wamp64\\www\\auction\\";
-$MD5_PREFIX = "02bda76179963068809af66db339d6f2";
-?>
